6d781a56555507403f11702485bab662a47b847a
t5maja03 <t5maja03@students.oamk.fi>
2017-11-20 17:21:53 +0200

    Added www site with current temp and chart

---
+++ www/config.js
@@ -0,0 +1,19 @@
+// host = '172.16.153.122';	// hostname or IP address
+host = 'mqtt.datakolmio.net';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+port = 9001;
+topic = 'testi';		// topic to subscribe to
+useTLS = false;
+username = "t5maja03";
+password = "tvt15smo";
+// username = "jjolie";
+// password = "aa";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
